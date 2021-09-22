442635ac81423e2593c38a229d0f8f0d6033979b
David Calhoun <438664+dcalhoun@users.noreply.github.com>
2021-09-22 14:03:59 -0400

    [wip] Explore Redux analytics middleware
    Appears to fire at the correct time. Currently it merely logs a message.

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import {
 	doGutenbergNativeSetup,
 	initialHtmlGutenberg,
 } from '@wordpress/react-native-editor';
+import { use } from '@wordpress/data';
 
 /**
  * Internal dependencies
@@ -48,5 +49,64 @@ addFilter( 'native.block_editor_props', 'gutenberg-mobile', ( editorProps ) => {
 	}
 	return editorProps;
 } );
+
+const REDUX_TRACKING = {
+	'core/block-editor': {
+		moveBlocksUp: 'test',
+		moveBlocksDown: 'test',
+		removeBlocks: 'test',
+		removeBlock: 'test',
+		moveBlockToPosition: 'test',
+		insertBlock: 'test',
+		insertBlocks: 'test',
+		replaceBlock: 'test',
+		replaceBlocks: 'test',
+		replaceInnerBlocks: 'test',
+	},
+};
+
+function tracksRecordEvent( params ) {
+	console.log( '>>> tracksRecordEvent', params );
+}
+
+use( ( registry ) => ( {
+	dispatch: ( namespace ) => {
+		const namespaceName =
+			typeof namespace === 'object' ? namespace.name : namespace;
+		const actions = { ...registry.dispatch( namespaceName ) };
+		const trackers = REDUX_TRACKING[ namespaceName ];
+
+		if ( trackers ) {
+			Object.keys( trackers ).forEach( ( actionName ) => {
+				const originalAction = actions[ actionName ];
+				const tracker = trackers[ actionName ];
+				actions[ actionName ] = ( ...args ) => {
+					console.log(
+						'action "%s" called with %o arguments',
+						actionName,
+						[ ...args ]
+					);
+					// We use a try-catch here to make sure the `originalAction`
+					// is always called. We don't want to break the original
+					// behaviour when our tracking throws an error.
+					try {
+						if ( typeof tracker === 'string' ) {
+							// Simple track - just based on the event name.
+							tracksRecordEvent( tracker );
+						} else if ( typeof tracker === 'function' ) {
+							// Advanced tracking - call function.
+							tracker( ...args );
+						}
+					} catch ( err ) {
+						// eslint-disable-next-line no-console
+						console.error( err );
+					}
+					return originalAction( ...args );
+				};
+			} );
+		}
+		return actions;
+	},
+} ) );
 
 doGutenbergNativeSetup();
